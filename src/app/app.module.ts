18184c81a0347b1eaf3e933ae1ddbe9c266c25d8
Ankansa <ankan.sark@gmail.com>
2022-08-16 16:14:13 +0530

    [Ankan]:Added login & registration fecility with data validation and api intriration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+
+
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationComponent } from './component/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/registration/registration.component.ts
@@ -0,0 +1,100 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CustomerService } from 'src/app/Service/Customer/customer.service';
+
+
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+
+  hide = true;
+  changeView = false;
+  
+
+  registerForm: FormGroup;
+  loginForm: FormGroup;
+  submitted = false;
+
+
+
+
+  constructor(private formBuilder: FormBuilder, private customerService: CustomerService,private snakbar: MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      FullName: ['', Validators.required],
+      Email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      mobileNumber: ['', Validators.required]
+
+
+      
+    });
+
+    this.loginForm = this.formBuilder.group({
+      Email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  view() {
+    console.log("View function called")
+    this.changeView = true;
+    // this.viewColor="warn";
+
+  }
+  view2() {
+    console.log("View function called")
+    this.changeView = false;
+    // this.viewColor="warn";
+
+  }
+
+  
+
+  registration() {
+    console.log("registration called")
+    if (this.registerForm.valid) {
+      console.log("The input registration data is: ", this.registerForm.value)
+
+      this.customerService.registration(this.registerForm.value).subscribe((responce: any) => {
+        console.log("The registration responce is :",responce)
+
+        this.snakbar.open('Registration Sucessfull', 'Ok', {duration: 4000
+        });
+        
+      },(error: any) => {
+        console.log("The error is: ", error);
+        this.snakbar.open(error.error.message, 'Ok', {
+        });
+      })
+    }
+  }
+
+  login() {
+    console.log("login called")
+    if (this.loginForm.valid) {
+      console.log("The input login data is :", this.loginForm.value)
+      this.customerService.login(this.loginForm.value).subscribe((responce: any) => {
+        console.log("The login responce is :",responce)
+        localStorage.setItem('token', responce.data);
+        this.snakbar.open('Login Sucessfull', 'Ok', {duration: 4000
+        });
+        
+      },(error: any) => {
+        console.log("The error is: ", error);
+        this.snakbar.open(error.error.message, 'Ok', {
+        });
+      })
+    }
+  }
+
+
+
+
+}
